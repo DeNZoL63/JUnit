b74f7896fa5c82fd55ab2d01ee41d1bd225dc0d5
Denis Khrapov <khrapov@haulmont.com>
2020-02-10 11:22:50 +0400

    arguments test

--- src/test/java/com/haulmont/task/MailServiceTest.java
+++ src/test/java/com/haulmont/task/MailServiceTest.java
@@ -1,5 +1,6 @@
 package com.haulmont.task;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -10,19 +11,40 @@ public class MailServiceTest {
 
     private MailSender mailSender = Mockito.mock(MailSender.class);
     private MessageRepository messageRepository = Mockito.mock(MessageRepository.class);
+    private Message message;
+    private MailService mailService;
 
-    @Test
-    public void simpleTest() {
+    @Before
+    public void beforeTests() {
         Person personFrom = new Person();
         personFrom.setEmail("sender@mail.ru");
         Person personTo = new Person();
         personTo.setEmail("getter@mail.ru");
-        Message message = new Message("", personFrom, personTo, "");
 
-        MailService mailService = new MailService(mailSender, messageRepository);
-        Mockito.when(mailSender.sendMessage(anyString(), anyString(), anyString(), anyString())).thenReturn(true);
+        message = new Message("testSubject", personFrom, personTo, "testText");
+        mailService = new MailService(mailSender, messageRepository);
+    }
+
+    @Test
+    public void simpleTest() {
+        Mockito
+                .when(mailSender.sendMessage(anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(true);
 
         boolean result = mailService.sendMessage(message);
         assertTrue(result);
     }
+
+    @Test
+    public void argumentsTest() {
+
+        Mockito
+                .when(mailSender.sendMessage(message.getFrom().getEmail(), message.getTo().getEmail(),
+                        message.getSubject(), message.getText()))
+                .thenReturn(true);
+
+        boolean result = mailService.sendMessage(message);
+        assertTrue(result);
+    }
+
 }
